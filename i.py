7bfb1c86023759a1b4f73e111fc4c0abce2b6ddd
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2022-11-18 20:13:52 +0300

    add

---
+++ i.py
@@ -0,0 +1,14 @@
+
+import streamlit as st
+from transformers import pipeline
+
+st.header("Определение тональности текстов")
+st.subheader("Введите текст для анализа")
+
+text = st.text_area(" ",height=50)
+
+classifier = pipeline("sentiment-analysis",   
+                      "blanchefort/rubert-base-cased-sentiment")
+
+
+st.write(classifier(text)) #странный вывод!
