99b8e6ae7b4469ae270d3a4559ec41a260ede708
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2022-12-07 23:12:16 +0300

    add

--- code.py
+++
@@ -1,6 +0,0 @@
-!pip install transformers sentencepiece sacremoses
-from transformers import pipeline
-classifier = pipeline("sentiment-analysis",   
-                      "blanchefort/rubert-base-cased-sentiment")
-x =  input("Введите текст ")
-classifier(x)
