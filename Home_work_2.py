22a57090ce5e43fb11254db49e73a68361c9cd61
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2022-12-07 19:20:15 +0300

    Update Home_work_2.py

--- Home_work_2.py
+++ Home_work_2.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 import seaborn as sns
 import streamlit as st
+import matplotlib.pyplot as plt
 
 st.header("Определение тональности текстов")
 st.subheader("Введите текст для анализа")
