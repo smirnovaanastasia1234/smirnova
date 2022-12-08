563485355294df8d3e958f89e8441a97252c1ac3
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2022-12-08 23:20:27 +0300

    фвв

--- HW4.py
+++ HW4.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 import seaborn as sns
 import streamlit as st
-import matplotlib.pyplot as plt
+import transformers
 from transformers import pipeline
 
 
