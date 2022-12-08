cb62c7995a1ebbf62199420072c55f313aba8e9a
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2022-12-08 22:58:57 +0300

    add

---
+++ HW4.py
@@ -0,0 +1,21 @@
+
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt 
+import seaborn as sns
+import streamlit as st
+import matplotlib.pyplot as plt
+from transformers import pipeline
+
+
+
+st.header("Определение тональности текстов")
+st.subheader("Введите текст для анализа")
+
+text = st.text_area(" ",height=100)
+
+classifier = pipeline("sentiment-analysis",   
+                      "blanchefort/rubert-base-cased-sentiment")
+st.write ("Тональность текста:")
+st.write(classifier(text)[0]["label"])
+#end program 
