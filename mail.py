fa78aadf71965fc9f4bf03d9150672fc739620af
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2022-12-09 20:18:48 +0300

    Update mail.py

--- mail.py
+++ mail.py
@@ -2,11 +2,10 @@ from fastapi import FastAPI
 from transformers import pipeline
 from pydantic import BaseModel
 
-
 class Item(BaseModel):
     text: str
 
-app = FastAPI(title= "Определение тональности текстов на русском языке")
+app = FastAPI(title= "Определение тональности текстов")
 classifier = pipeline("sentiment-analysis","blanchefort/rubert-base-cased-sentiment")
 
 @app.get("/")
