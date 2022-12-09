fb97fd9536e8c88d7275401f9950d805ed87e8e3
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2022-12-09 20:01:46 +0300

    Update Home_work_2.py

--- Home_work_2.py
+++ Home_work_2.py
@@ -1,10 +1,8 @@
 
 import numpy as np 
 import pandas as pd
-import matplotlib.pyplot as plt 
 import seaborn as sns
 import streamlit as st
-import matplotlib.pyplot as plt
 from transformers import pipeline
 
 
