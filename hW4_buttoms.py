d5138c4c5c7bb68cf9e830380c21be01ad7784cc
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2022-12-09 09:37:12 +0300

    add

---
+++ hW4_buttoms.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt 
+import seaborn as sns
+import streamlit as st
+import transformers
+from transformers import pipeline
+
+
+
+st.header("Определение тональности текстов")
+st.subheader("Введите текст для анализа")
+
+text = st.text_area(" ",height=100)
+
+classifier = pipeline("sentiment-analysis",   
+                      "blanchefort/rubert-base-cased-sentiment")
+                 
+st.write ("Тональность текста:")
+st.write(classifier(text)[0]["label"])
+#end program 
