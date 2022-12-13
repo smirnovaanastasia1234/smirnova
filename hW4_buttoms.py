d62fb117dcd5c6742e30fcc67d33ab4135cfec43
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2022-12-13 11:51:51 +0300

    Update hW4_buttoms.py

--- hW4_buttoms.py
+++ hW4_buttoms.py
@@ -2,16 +2,18 @@ import streamlit as st
 from transformers import pipeline
 import torch
 
-@st.cache
+
 
 st.header("Определение тональности текстов")
 st.subheader("Введите текст для анализа")
+
+@st.cache
 
 text = st.text_area(" ",height=100)
 
 classifier = pipeline("sentiment-analysis",   
                       "blanchefort/rubert-base-cased-sentiment")
-if st.button("Определить тональность текста"):
-   st.write ("Тональность текста:")
-   st.write(classifier(text)[0]["label"])
+
+st.write ("Тональность текста:")
+st.write(classifier(text)[0]["label"])
 #end program 
