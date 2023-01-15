107e549daacd95f6a5040f14c29f51d158ae3abd
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-01-16 01:07:03 +0300

    Changes

--- tg.py
+++ tg.py
@@ -2,39 +2,18 @@ import streamlit as st
 import pandas as pd
 from io import StringIO
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
+# Настройка заголовка и текста 
+st.title("COVID 19 IN THE WORLD DASHBOARD")
+st.write("""This dashboard will present the spread of COVID-19 in the world by visualizing the timeline of the total cases and deaths. As well as the total number of vaccinated people.""")
 
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
-
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
-
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
-
-@st.cache(allow_output_mutation=True)
-
-def load_model():
-    model=pipeline("sentiment-analysis",   
-                      "blanchefort/rubert-base-cased-sentiment")
-    return model
-
-model = load_model()
-st.header ("Определение тональности текстов")
-st.subheader ("Введите текст для анализа")
-text = st.text_area(" ",height=100)
-result = st.button("Определить тональность текста")
-
-
-if result:
-    res = model(text)
-    sent = res[0]['label'] 
-    st.write(model(text)[0]["label"])
+# Настройка боковой панели
+st.sidebar.title("About")
+st.sidebar.info(
+    """
+    This app is Open Source dashboard.
+    """
+)
+st.sidebar.info("Feel free to collaborate and comment on the work. The github link can be found "
+                "[here](https://github.com/yuliianikolaenko/COVID_dashboard_proglib).")
+# Загружаем новые оптимизированные данные
+DATA = ('data.csv')
