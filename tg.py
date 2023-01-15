fa14ae28e78cd592a904a05b4cd8b0fd49a8e091
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-01-16 01:02:53 +0300

    changes

--- tg.py
+++ tg.py
@@ -20,3 +20,21 @@ if uploaded_file is not None:
     dataframe = pd.read_csv(uploaded_file)
     st.write(dataframe)
 
+@st.cache(allow_output_mutation=True)
+
+def load_model():
+    model=pipeline("sentiment-analysis",   
+                      "blanchefort/rubert-base-cased-sentiment")
+    return model
+
+model = load_model()
+st.header ("Определение тональности текстов")
+st.subheader ("Введите текст для анализа")
+text = st.text_area(" ",height=100)
+result = st.button("Определить тональность текста")
+
+
+if result:
+    res = model(text)
+    sent = res[0]['label'] 
+    st.write(model(text)[0]["label"])
