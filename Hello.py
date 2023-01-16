93082b26843d0b98fb166f607b59da0f90a33df8
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-01-16 09:52:39 +0300

    add

--- Hello.py
+++ Hello.py
@@ -9,6 +9,3 @@ st.set_page_config(
 
 st.subheader("Группа 32: Смирнова А., Кожедуб Н., Багаудинов Э., Петраков В.")
 
-display = Image.open('images/display.jpg')
-display = np.array(display)
-st.image(display)
