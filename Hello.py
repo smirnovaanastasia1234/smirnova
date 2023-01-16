28878228f1e86ac65f9c683aa3883325a7a2cda7
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-01-16 09:54:14 +0300

    changes

--- Hello.py
+++ Hello.py
@@ -4,8 +4,10 @@ from transformers import pipeline
 from PIL import  Image
 st.set_page_config(
     page_title="Анализ тональности текста",
-    page_icon="👋",
+    page_icon="👋",)
 # Title of the application 
+st.write("# Добро пожаловать! 👋")
 
+st.sidebar.success("Select a demo above.")
 st.subheader("Группа 32: Смирнова А., Кожедуб Н., Багаудинов Э., Петраков В.")
 
