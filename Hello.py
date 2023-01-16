6837f8c4f3c465fdc7fe1edcae052635d51c4433
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-01-16 09:49:10 +0300

    add

--- Hello.py
+++ Hello.py
@@ -2,7 +2,9 @@ import numpy as np
 import streamlit as st
 from transformers import pipeline
 from PIL import  Image
-
+st.set_page_config(
+    page_title="Анализ тональности текста",
+    page_icon="👋",
 # Title of the application 
 st.title('Анализ тональности текста\n', )
 st.subheader("Группа 32: Смирнова А., Кожедуб Н., Багаудинов Э., Петраков В.")
