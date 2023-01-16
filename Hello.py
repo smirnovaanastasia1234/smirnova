62b077bd6798159655cd984c9c1409131b1f8a50
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2023-01-16 09:50:02 +0300

    add

--- Hello.py
+++ Hello.py
@@ -6,7 +6,7 @@ st.set_page_config(
     page_title="Анализ тональности текста",
     page_icon="👋",
 # Title of the application 
-st.title('Анализ тональности текста\n', )
+
 st.subheader("Группа 32: Смирнова А., Кожедуб Н., Багаудинов Э., Петраков В.")
 
 display = Image.open('images/display.jpg')
